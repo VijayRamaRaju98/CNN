171c7aee5b45653193925653870708a48fb141be
Vijaya Rama Raju Uppalapati <uppalapativijay98@gmail.com>
2023-10-27 19:50:33 +0530

    all requrements added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+
+
+
+setup(
+    name='potato_cnn',
+    version='0.0.0',
+    author='VijayRamaRaju98',
+    author_email='uppalapativijay98@gmail.com',
+    packages=find_packages()
+    
+)
---
+++ src/potato_cnn/test.py
@@ -0,0 +1,4 @@
+import shutil
+
+
+shutil.copy2("C:\\Users\\Vijay Rama Raju U\\datasets\\deeplearning\\potato_leafs","C:\\Users\\Vijay Rama Raju U\\datasets\\vijay\\all projects\CNN\\artifacts")
